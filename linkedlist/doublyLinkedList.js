486fb7699faaef7aa98a03ee669fc69b5fdd2b6b
Ishan <ishan.108@gmail.com>
2024-07-30 13:16:00 -0500

    Doubly Linked List

---
+++ linkedlist/doublyLinkedList.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(data, prev = null, next = null) {
+    this.data = data;
+    this.prev = prev;
+    this.next = next;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
