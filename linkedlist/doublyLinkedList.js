06b8cbba9a16910dbf20c476f702156db18f33de
Ishan <ishan.108@gmail.com>
2024-07-31 00:44:00 -0500

    Doubly linked list delete first node

--- linkedlist/doublyLinkedList.js
+++ linkedlist/doublyLinkedList.js
@@ -55,3 +55,16 @@ DoublyLinkedList.prototype.insertAfterGivenNode = function (data, prevNode) {
     this.tail = newNode;
   }
 };
+
+DoublyLinkedList.prototype.deleteFirstNode = function () {
+  if (!this.head) {
+    return; // Nothing to delete
+  }
+  if (this.head === this.tail) {
+    this.head = null;
+    this.tail = null;
+  } else {
+    this.head = this.head.next;
+    this.head.prev = null;
+  }
+};
