d4b95507340f644d6129f9fff002c0f89e4b1699
Ishan <ishan.108@gmail.com>
2024-08-03 23:48:14 -0500

    Check Palindrom with Recursion

---
+++ Problems/Recursion/palindrome.js
@@ -0,0 +1,10 @@
+function palindrome(string) {
+  if (string.length <= 1) {
+    return true;
+  }
+  if (string[0] == string[string.length - 1]) {
+    return palindrome(string.substring(1, string.length - 1));
+  }
+  return false;
+}
+console.log(palindrome("madam"));
