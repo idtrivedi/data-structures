4c82f6cf779a91e12330d5f38aeed2f21a7505fa
Ishan <ishan.108@gmail.com>
2024-08-05 01:08:34 -0500

    Graph BFS traversal

---
+++ graph/breadthFirstSearch.js
@@ -0,0 +1,23 @@
+const graph = {
+  a: ["b", "c"],
+  b: ["a", "c", "d"],
+  c: ["a", "b", "d"],
+  d: ["b", "c"],
+  e: ["f"],
+  f: ["e"],
+};
+
+function bfsGraphIterative(graph, start) {
+  const queue = [start];
+  const visited = new Set();
+
+  while (queue.length() > 0) {
+    const node = queue.shift();
+    for (const neighbor of graph[node]) {
+      if (!visited.has(neighbor)) {
+        visited.add(neighbor);
+        queue.push(neighbor);
+      }
+    }
+  }
+}
